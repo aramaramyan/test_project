d4aa0da41060d3e05df23c30c7acfe4d619631c0
Aram Aramyan <aramaramyan1997@mail.ru>
2022-02-02 18:52:52 +0400

    feat(script.js): create heading section

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ function newElement(tagName, attributes, childs) {
 
   return element;
 }
+
+// HEADER SECTION START
+
+function createHeader() {
+  const logo = newElement("img", {src: "./img/logo.png", alt: "Logo"}, []),
+    headingName = newElement("div", {class: "heading_name"}, [
+      newElement("h1", {}, ["MAGNAT"]),
+      newElement("h3", {}, ["HOOKAH"]),
+    ]),
+    logoWrapper = newElement("div", {class: "logo_wrapper"}, [logo, headingName]),
+    home = newElement("a", {href: "#header"}, ["HOME"]),
+    gallery = newElement("a", {href: "#gallery"}, ["GALLERY"]),
+    partners = newElement("a", {href: "#partners"}, ["PARTNERS"]),
+    order = newElement("a", {href: "#oredr"}, ["ORDER"]),
+    navUl = newElement("ul", {class: "navbar_header_ul"}, [
+      newElement("li", {class: "navbar_header_li"}, [home]),
+      newElement("li", {class: "navbar_header_li"}, [gallery]),
+      newElement("li", {class: "navbar_header_li"}, [partners]),
+      newElement("li", {class: "navbar_header_li"}, [order]),
+    ]),
+    navbar = newElement("nav", {class: "navbar_header"}, [navUl]);
+
+  return newElement("header", {id: "header"}, [logoWrapper, navbar]);
+}
+
+// HEADER SECTION END
+
+root.append(createHeader());
